93c028c3ca9efbb526149463f606c79ece9f413c
lavrelin <pwnzartemlavrelin@gmail.com>
2025-10-19 18:53:58 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Integer, BigInteger, String, DateTime, Boolean, Text, ForeignKey, Enum as SQLEnum
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import Column, Integer, BigInteger, String, DateTime, Boolean, Text, JSON, Enum as SQLEnum
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from datetime import datetime
 from enum import Enum
 
