dd8a00a5b7cbf58d9867df7c4220e6935cb107db
lavrelin <pwnzartemlavrelin@gmail.com>
2025-10-26 01:01:51 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,7 +2,7 @@
 # Unified: social, bonus, links, medicine -> info_handler (prefix: ifc_)
 
 from .start_handler import start_command,
-help_command, show_main_menu, show_write_menu
+show_main_menu, show_write_menu
 )
 from .menu_handler import handle_menu_callback
 )
